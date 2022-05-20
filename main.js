73f0ec3cabb035049e66a24d2693049c9f68ba81
meduna femi <femimeduna@gmail.com>
2022-05-20 15:10:52 -0600

    fix audio

--- main.js
+++ main.js
@@ -28,10 +28,7 @@ const servers = {
 
 
 const constraints = {
-    video: {
-        width: { min: 640, ideal: 1920, max: 1920 }, 
-        height: { min: 480, ideal: 1080, max: 1080 }, 
-    },
+    video: true
     audio: true
 }
 
